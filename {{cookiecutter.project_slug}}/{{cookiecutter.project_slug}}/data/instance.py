2597be58e15c017cc50ddd480fb3ca5c4fe2d81e
xvr.hlt <holt.xavier@gmail.com>
2020-09-02 20:12:11 +1000

    fix something

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/data/instance.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/data/instance.py
@@ -8,15 +8,16 @@ class Instance:
     mask: str{%endif %}{%if cookiecutter.text %}
     text: str{%endif %}
 
-def get_train_val_instances(val_split=0.2, split_ix=0):
+def get_train_val_instances(n_folds: int, val_fold: int = 0):
     instances = []
 
     def hash_str(s):
         return int(hashlib.sha512(s.encode()).hexdigest(), 16)
 
-    instances = sorted(instances, key=lambda i: hash_str(i.uid))
-    split_size = int(val_split * len(instances))
-    val_start, val_end = split_size * split_ix, split_size * (split_ix + 1)
-    val_instances = instances[val_start:val_end]
-    train_instances = instances[:val_start] + instances[val_end:]
+    train_instances, val_instances = [], []
+    for i in instances:
+        if hash_str(i.uid) % n_folds == val_fold:
+            val_instances.append(i)
+        else:
+            train_instances.append(i)
     return train_instances, val_instances
